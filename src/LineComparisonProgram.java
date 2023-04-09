2d0722eeba6bab2d4c2112222ab39981de88809f
iAmMujeeb <94049888+iAmMujeeb@users.noreply.github.com>
2023-04-09 15:14:11 +0530

    Update LineComparisonProgram.java

--- src/LineComparisonProgram.java
+++ src/LineComparisonProgram.java
@@ -1,12 +1,34 @@
-public class LineComparisonProgram {
+public class LineComparisonProblem {
     public static void main(String[] args) {
-        System.out.println("welcome to line comparison");
-        int x1=3;
-        int x2=3;
-        int y1=2;
-        int y2=4;
-        double l = 0;
-        l = (Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
-        System.out.println(l);
+        int x1=11;
+        int x2=6;
+        int y1=7;
+        int y2=31;
+        double l=0;
+        double l1=0;
+        double l2=0;
+        for(int i=1; i<=2; i++) {
+            l = (Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+            System.out.println(l);
+            if(i==1)
+                l1=l;
+            else
+                l2=l;
+        }
+        String s1= String.valueOf(l1);
+        String s2= String.valueOf(l2);
+        Integer l3=(int)l1;
+        Integer l4=(int)l2;
+        boolean Result = l3.equals(l4);
+        if(Result==true)
+            System.out.println("Lines are equal");
+        else
+            System.out.println("Lines are not equal");
+        if((s1.compareTo(s2))==0)
+            System.out.println("Lines are equal");
+        else
+            System.out.println("Lines are not equal");
+
+
     }
 }
